ebb15b9e76f96dc5f5452329bf9dde7681f4adf4
jmichael2025 <jasminmichael8@gmail.com>
2026-09-23 15:17:44 +0100

    Add recipe author relationship

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,6 +13,7 @@ class Recipe(models.Model):
     ingredients = models.TextField()
     instructions = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.name
